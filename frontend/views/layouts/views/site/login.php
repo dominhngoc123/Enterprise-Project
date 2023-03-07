87caaa729967057512f46d89fd4b693d243fb6cd
John Constantine <ngocatp.52@gmail.com>
2023-03-08 00:10:45 +0700

    Fix login view

--- frontend/views/layouts/views/site/login.php
+++ frontend/views/layouts/views/site/login.php
@@ -1,64 +1,53 @@
 <?php
+
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
 ?>
-<div class="card">
-    <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign in to start your session</p>
 
-        <?php $form = \yii\bootstrap4\ActiveForm::begin(['id' => 'login-form']) ?>
-
-        <?= $form->field($model,'username', [
-            'options' => ['class' => 'form-group has-feedback'],
-            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
-            'template' => '{beginWrapper}{input}{error}{endWrapper}',
-            'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])
-            ->label(false)
-            ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
-
-        <?= $form->field($model, 'password', [
-            'options' => ['class' => 'form-group has-feedback'],
-            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
-            'template' => '{beginWrapper}{input}{error}{endWrapper}',
-            'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])
-            ->label(false)
-            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
-
+<div class="content">
+    <div class="container">
         <div class="row">
-            <div class="col-8">
-                <?= $form->field($model, 'rememberMe')->checkbox([
-                    'template' => '<div class="icheck-primary">{input}{label}</div>',
-                    'labelOptions' => [
-                        'class' => ''
-                    ],
-                    'uncheck' => null
-                ]) ?>
+            <div class="col-md-6">
+                <img src="../images/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
             </div>
-            <div class="col-4">
-                <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary btn-block']) ?>
+            <div class="col-md-6 contents justify-content-center">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="mb-4">
+                            <h3 class="text-center">Staff sign in</h3>
+                        </div>
+                        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                        <div class="form-group first">
+                            <?= $form->field($model, 'username', [
+                                'options' => ['class' => 'form-control'],
+                                'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                'wrapperOptions' => ['class' => 'input-group mb-3 mt-3']
+                            ])->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+
+                        </div>
+                        <div class="form-group last mb-4">
+                            <?= $form->field($model, 'password', [
+                                'options' => ['class' => 'form-control'],
+                                'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                'wrapperOptions' => ['class' => 'input-group mb-3 mt-3']
+                            ])->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+
+                        </div>
+
+                        <div class="d-flex mb-1 align-items-center mb-4">
+                            <?= $form->field($model, 'rememberMe')->checkbox([
+                                'template' => '<div class="icheck-primary">{input}&nbsp;{label}</div>',
+                                'uncheck' => null
+                            ]) ?>
+                        </div>
+
+                        <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary btn-block w-100']) ?>
+                        <?php ActiveForm::end(); ?>
+                    </div>
+                </div>
+
             </div>
+
         </div>
-
-        <?php \yii\bootstrap4\ActiveForm::end(); ?>
-
-        <div class="social-auth-links text-center mb-3">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-primary">
-                <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-            </a>
-            <a href="#" class="btn btn-block btn-danger">
-                <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-            </a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-        </p>
-        <p class="mb-0">
-            <a href="register.html" class="text-center">Register a new membership</a>
-        </p>
     </div>
-    <!-- /.login-card-body -->
 </div>
